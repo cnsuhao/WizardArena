ed51ae3b2b86038c13f75abadf774b66c9e9cc8f
Anas Idiab <Candfa2660@gmail.com>
2018-04-11 12:09:07 +0200

    Working SceneManager class

---
+++ src/scenemanager.cpp
@@ -0,0 +1,30 @@
+#include "scenemanager.hpp"
+
+SceneManager::SceneManager(Scene* PrimaryScene) {
+  scenes = vector<Scene*>();
+  scenes.push_back(PrimaryScene);
+}
+
+SceneManager::~SceneManager() {
+  for (auto scene : scenes) { delete scene; }
+  scenes.clear();
+}
+
+void SceneManager::ChangePrimaryScene(Scene* scene) {
+  if (scenes.size() > 0) {
+    delete scenes[0];
+    scenes[0] = scene;
+  } else {
+    scenes.push_back(scene);
+  }
+}
+
+void SceneManager::AddScene(Scene* scene) { scenes.push_back(scene); }
+
+void SceneManager::Draw() {
+  for (auto scene : scenes) { scene.Draw(); }
+}
+
+void SceneManager::Update() {
+  for (auto scene : scenes) { scene.Update(); }
+}
--- src/scenemanager.hpp
+++ src/scenemanager.hpp
@@ -1,15 +1,16 @@
 #ifndef SCENEMANAGER_HPP
 #define SCENEMANAGER_HPP
 #include "includes.hpp"
-
-typedef int Scene;
+#include "scene.hpp"
 
 class SceneManager {
  public:
-  SceneManager();
+  SceneManager(Scene* PrimaryScene);
   virtual ~SceneManager();
-  void ChangePrimaryScene(Scene* Scene);
-  void AddScene(Scene* Scene);
+  void ChangePrimaryScene(Scene* scene);
+  void AddScene(Scene* scene);
+  void Draw();
+  void Update();
 
  private:
   vector<Scene*> scenes;
