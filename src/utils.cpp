6c98b2249a8ed033826d89a187316efb3d7a276d
Anas Youssef Idiab <candfa2660@gmail.com>
2018-05-07 22:00:31 +0200

    Fix multiple definition error on some systems.

---
+++ src/utils.cpp
@@ -0,0 +1,13 @@
+#include "utils.hpp"
+
+/**
+   Point-rectangle intersection
+*/
+bool PointRectIntersect(vec2 point, vec2 size, vec2 center) {
+  if (center.x + (size.x / 2) > point.x && center.x - (size.x / 2) < point.x &&
+      center.y + (size.y / 2) > point.y && center.y - (size.y / 2) < point.y) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- src/utils.hpp
+++ src/utils.hpp
@@ -14,14 +14,6 @@ T clamp(T val, T min, T max) {
 /**
   Point-rectangle intersection
  */
-
-bool PointRectIntersect(vec2 point, vec2 size, vec2 center) {
-  if (center.x + (size.x / 2) > point.x && center.x - (size.x / 2) < point.x &&
-      center.y + (size.y / 2) > point.y && center.y - (size.y / 2) < point.y) {
-    return true;
-  } else {
-    return false;
-  }
-}
+bool PointRectIntersect(vec2 point, vec2 size, vec2 center);
 
 #endif  // UTILS_H
