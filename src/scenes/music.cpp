0578a5534d7d696d0dbe4f0e3a0b50158a8287b9
Anas Idiab <Candfa2660@gmail.com>
2018-05-07 21:28:17 +0200

    Remove mp3 support. Switch to OGG vorbis.

--- src/scenes/music.cpp
+++ src/scenes/music.cpp
@@ -1,7 +1,7 @@
 #include "music.hpp"
 
 void Music::Init() {
-  Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG);
+  Mix_Init(MIX_INIT_OGG);
   if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1) {
     printf("Mix_OpenAudio: %s\n", Mix_GetError());
     GameObject::globals->gameState = QUIT;
@@ -10,8 +10,7 @@ void Music::Init() {
 
 Music::Music() {
   MenuMusic = Mix_LoadMUS("Content/Music/Menu.ogg");
-  printf("MenuMusic: %s\n", Mix_GetError());
-  GameMusic = Mix_LoadMUS("Content/Music/Menu.mp3");
+  GameMusic = Mix_LoadMUS("Content/Music/Menu.ogg");
   Mix_PlayMusic(MenuMusic, -1);
 }
 
