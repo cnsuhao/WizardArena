407eaf62a109b60717a48813f6e4d13eeff947d6
Anas Youssef Idiab <candfa2660@gmail.com>
2018-05-07 21:24:42 +0200

    ogg test

--- src/scenes/music.cpp
+++ src/scenes/music.cpp
@@ -1,7 +1,7 @@
 #include "music.hpp"
 
 void Music::Init() {
-  Mix_Init(MIX_INIT_MP3);
+  Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG);
   if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1) {
     printf("Mix_OpenAudio: %s\n", Mix_GetError());
     GameObject::globals->gameState = QUIT;
@@ -9,7 +9,8 @@ void Music::Init() {
 }
 
 Music::Music() {
-  MenuMusic = Mix_LoadMUS("Content/Music/Menu.mp3");
+  MenuMusic = Mix_LoadMUS("Content/Music/Menu.ogg");
+  printf("MenuMusic: %s\n", Mix_GetError());
   GameMusic = Mix_LoadMUS("Content/Music/Menu.mp3");
   Mix_PlayMusic(MenuMusic, -1);
 }
