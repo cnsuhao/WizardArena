6a970e2610ee6a565404b7f4513c68c00b1f9686
Sebastian Winding <sebastiansmail4689@gmail.com>
2018-05-07 19:27:28 +0200

    Added Point-rectangle intersection

--- src/utils.hpp
+++ src/utils.hpp
@@ -10,4 +10,17 @@ T clamp(T val, T min, T max) {
   return val;
 }
 
+/**
+  Point-rectangle intersection
+ */
+
+bool PointRectIntersect(vec2 point, vec2 size, vec2 center) {
+  if (center.x + (size.x / 2) > point.x && center.x - (size.x / 2) < point.x &&
+      center.y + (size.y / 2) > point.y && center.y - (size.y / 2) < point.y) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 #endif  // UTILS_H
