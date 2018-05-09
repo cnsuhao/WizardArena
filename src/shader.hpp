7055a9ab67de4062d691762d7b16ba6cd3081766
Anas Youssef Idiab <Candfa2660@gmail.com>
2018-05-09 13:55:24 +0200

    Add uniforms to shader interface

--- src/shader.hpp
+++ src/shader.hpp
@@ -4,12 +4,15 @@
 
 class Shader {
  public:
-  Shader(string shaderName);
+  Shader(string shaderName, vector<string> uniformNames);
   ~Shader();
 
-  void Activate();
+  std::map<string, uint> GetUniforms() { return uniforms; };
+  void                   Activate();
+  static void            Deactivate();
 
-  static void Deactivate();
+ private:
+  std::map<string, uint> uniforms;
 };
 
 #endif  // SHADER_H
